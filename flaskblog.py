484413d9ff833dbf4eb28c1d8f05d71d2ed4541a
nabil-mahmoor <nabil.mahmoor@gmail.com>
2023-12-07 22:03:47 +0530

    chapter 02

--- flaskblog.py
+++ flaskblog.py
@@ -1,17 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
 
+
+posts = [
+    {
+        'author': 'Vanell Blunt',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'May 16, 2020'
+    },
+    {
+        'author': 'Zack Beer',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 29, 2021'
+    }
+]
+
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return 'Home Page'
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return 'About Page'
+    return render_template('home.html', title='About')
 
 
 if __name__ == '__main__':
